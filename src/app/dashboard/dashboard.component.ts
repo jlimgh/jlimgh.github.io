328e19a229a10def09ea160a49afc160a9b972b8
Jefferson Lim <jefferer@gmail.com>
2023-05-25 23:46:41 -0700

    Update styles, template data, and added assets

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -38,7 +38,7 @@ export class DashboardComponent implements OnInit {
       altImg: "Gojji Workqueue",
       imgLogo: "../../assets/gojji-logo.svg",
       altImgLogo: "Gojji Logo",
-      desc: `Front-end development of pilot and production apps for UI, agent dashboards, forms, and internal tools`,
+      desc: `Front-end development of a SaaS healthcare app that strives to manage chronic conditions easily.`,
       projectId: 'gojjiHealth',
     },
     {
@@ -68,7 +68,7 @@ export class DashboardComponent implements OnInit {
       altImg: "Specialty Pharmacy Workflow",
       imgLogo: null,
       altImgLogo: "Specialty Logo",
-      desc: `Specialty Pharmacy pilot for the use of pharmacists and clinicians. It keeps track of patient medications, orders, and insights.`,
+      desc: `Specialty Pharmacy SaaS pilot for the use of pharmacists and clinicians to help patients`,
       projectId: 'specialty'
     }
   ]
@@ -77,14 +77,15 @@ export class DashboardComponent implements OnInit {
     portfolio: {
       title: 'Portfolio',
       techStack: 'Angular, Material, Bootstrap',
-      images: ["../../assets/portfolio1.png"],
+      images: ['../../assets/portfolio1.png'],
+      repoLink: 'https://github.com/jlimgh/jlimgh.github.io',
       info: [
         `Portfolio website created with Angular and Material`,
       ]
     },
     gojjiHealth: {
       title: 'Gojji Health',
-      techStack: 'Angular, Typescript, Bootstrap, Node.js , MongoDB, Highcharts, RESTful',
+      techStack: 'Angular, Typescript, RxJS, Bootstrap, Node.js , MongoDB, Highcharts, S3',
       images: [
         '../../../assets/gojji1.png',
         '../../../assets/gojji3.png',
@@ -96,37 +97,42 @@ export class DashboardComponent implements OnInit {
       info: [
         `Successfully led front-end development of Gojji's pilot launch consisting of 200 initial enrollments that led to a user base in the thousands`,
         'Consistently met tight deadlines and solved critical requirements working with business analysts, designers, and project managers',
-        'Built SaaS dashboard and workqueue modules for both customer and business use, focusing on resuability and scalability',
-        'Key contributor for front-end performance tuning, maintenance, testing phases, and releases'
+        'Built SaaS dashboard and workqueue UI modules for both customer and business use, focusing on resuability and scalability',
+        'Used Reactive forms and state management through service Observables',
+        'Handled chaining multiple asyc calls through RxJS Observables',
       ]
     },
     loanDepot: {
       title: 'loanDepot',
-      techStack: 'Angular 12, NX, Typescript, NGRX, Jamstack, Bootstrap, Material, C#, Azure pipelines',
+      techStack: 'Angular (updated), NX, Kentico, NGRX, Jamstack, Bootstrap, Material, C#, Azure pipelines',
       images: [
-        '../../../assets/loandepot-card-portfolio.png'
+        '../../../assets/loandepot-card-portfolio.png',
+        '../../../assets/loandepot2.png'
       ],
       info: [
         `Supported development of loanDepot’s full catalog of pages and internal tools utilizing Angular, Jamstack, and C#`,
         `Built tools and UI features for internal NPM Libraries
-        regarding stylesheet, UI features, reactive forms, and reactive state management.`
+        regarding stylesheet, UI features, reactive forms, and reactive state management.`,
       ]
     }, 
     taskPostings: {
       title: 'Task Postings',
-      techStack: 'Angular 14, Reactive Form, Bootstrap, C#, .NET',
+      techStack: 'Angular 14, Bootstrap, Material, Node.js, MongoDB',
+      repoLink: 'https://github.com/jlimgh/task-postings',
       info: [
-        'Registration reactive form with custom validation example',
-        'Container component holds the logic, while the child component is a reusable form component',
-        'Client is hooked up to a local C# backend that responds with a mocked result'
+        'Project app that creates postings for tasks and gigs for users',
+        'Implemented forms, routing for different dashboards, and lazy loading modules',
+        'Created RESTful API used with Node.js and MongoDB backend'
       ],
       images: [
-        '../../../assets/task-posting1.png'
+        '../../../assets/task-posting1.png',
+        '../../../assets/task-posting2.png'
       ]
     },
     registrationForm: {
       title: 'Registration Form Example',
       techStack: 'Angular 14, Reactive Form, Bootstrap, C#, .NET 7',
+      repoLink: 'https://github.com/jlimgh/emdsregister',
       info: [
         'Registration reactive form with custom validation example',
         'Container component holds the logic, while the child component is a reusable form component',
@@ -137,8 +143,14 @@ export class DashboardComponent implements OnInit {
       ]
     },
     specialty: {
-      title: 'Specialty',
-      techStack: 'Angular 4, Typescript, Bootstrap, Node.js, MongoDB, Highcharts, RESTful',
+      title: 'Specialty Pharmacy',
+      techStack: 'Angular 4, Bootstrap, Node.js, MongoDB',
+      info: [
+        'SaaS for the use of pharmacists and clinicians to help patients',
+        'Implemented dashboard requirements for patient features and user workflows',
+        'Utilized reactive forms, RxJS, routing, and container and presentational components',
+        'One way data flow through the use of a data store class and observables inside a service'
+      ],
       images: [
         '../../../assets/specialty1.png',
         '../../../assets/specialty2.png',
